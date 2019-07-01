bffa952f7de40a8272fe9ff29ce1585cfe8c210f
Chris Alves <chrisae9@gmail.com>
2019-06-30 21:49:03 -0400

    Added favicon support

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,6 +24,39 @@ module.exports = {
         name: `images`,
         path: `${__dirname}/src/images`,
       },
+      
+    {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: "./src/stockcalculator.ico",
+
+      // WebApp Manifest Configuration
+      appName: null, // Inferred with your package.json
+      appDescription: null,
+      developerName: null,
+      developerURL: null,
+      dir: 'auto',
+      lang: 'en-US',
+      background: '#fff',
+      theme_color: '#fff',
+      display: 'standalone',
+      orientation: 'any',
+      start_url: '/?homescreen=1',
+      version: '1.0',
+
+      icons: {
+        android: true,
+        appleIcon: true,
+        appleStartup: true,
+        coast: false,
+        favicons: true,
+        firefox: true,
+        yandex: false,
+        windows: false
+      }
+    }
+  }
+      
     },
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-sharp`,
